a9b3f4ee501d6a6d98a7d33735a3ad1302d41315
Ali-Gorgani <ali.gorgani.fj@gmail.com>
2024-07-07 10:12:53 +0330

    add proto type for gPRC

--- token/paseto_maker.go
+++ token/paseto_maker.go
@@ -27,27 +27,29 @@ func NewPasetoMaker(symmetricKey string) (Maker, error) {
 	return maker, nil
 }
 
-	// CreateToken creates a new token for specific username and duration
-	func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
+// CreateToken creates a new token for specific username and duration
+func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, *Payload, error) {
 
-		payload, err := NewPayload(username, duration)
-		if err != nil {
-			return "", err
-		}
-		return maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", payload, err
 	}
 
-	// VerifyToken verifies if the token is valid or not
-	func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
-		payload := &Payload{}
+	token, err := maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
+	return token, payload, err
+}
 
-		err := maker.paseto.Decrypt(token, maker.symmetricKey, payload, nil)
-		if err != nil {
-			return nil, ErrInvalidToken
-		}
-		err = payload.Valid()
-		if err != nil {
-			return nil, err
-		}
-		return payload, nil
+// VerifyToken verifies if the token is valid or not
+func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+
+	err := maker.paseto.Decrypt(token, maker.symmetricKey, payload, nil)
+	if err != nil {
+		return nil, ErrInvalidToken
 	}
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
+	}
+	return payload, nil
+}
